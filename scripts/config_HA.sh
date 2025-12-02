5afc519ebe6dc18d626d3699b7b94f7bd14b3462
LeonardHM <86130244+LeonardHM@users.noreply.github.com>
2025-12-01 21:24:18 -0300

    Update config_HA.sh

--- scripts/config_HA.sh
+++ scripts/config_HA.sh
@@ -19,7 +19,8 @@ configurar_home_assistant() {
         sudo mkdir -p "$HA_CC_PATH"
 
         if [ ! -d "$HA_CC_PATH/TvTime" ]; then
-            sudo git clone https://github.com/Ghau/TvTime.git /tmp/tvtime_temp/ >/dev/null 2>&1 || {
+            sudo mkdir -p "$HA_CC_PATH/TvTime"
+            git clone https://github.com/Ghau/TvTime.git /tmp/tvtime_temp/ >/dev/null 2>&1 || {
                 print_log "$(log_error)" "$(echo_red "Falha ao clonar o repositório TvTime.")"
                 exit 1
             }
